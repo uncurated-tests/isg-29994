e306d64440b05f77bbc168ebc3034d86840c27fd
Steven <steven@ceriously.com>
2021-12-14 12:10:43 -0500

    Add page

---
+++ pages/[slug].js
@@ -0,0 +1,23 @@
+
+export const getStaticPaths = async () => ({
+  paths: ['/page1', '/page2'],
+  fallback: 'blocking',
+});
+  
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const date = new Date().toISOString().split('T')[1];
+  return {
+    props: { slug, date },
+    revalidate: 5,
+  };
+}
+
+export default function Page({ slug, date }) {
+  return (
+    <div>
+      <h1>{slug}</h1>
+      <p>Generated at <em>{date}</em></p>
+    </div>
+  );
+}
